fca5a1eb761e7d04d45fb85d233cb65756e83682
HuseynJafarov <huseynnj@code.edu.az>
2022-10-12 17:51:48 +0400

    LoadMore

---
+++ Partial views, Load more/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partial_views__Load_more.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
